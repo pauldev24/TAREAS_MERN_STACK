1bd288845a25d14dc319fd5631883d43f7501d01
Paul212405 <mallquiriverap@gmail.com>
2023-04-05 16:47:42 -0500

    Arreglo

--- server/db.js
+++ server/db.js
@@ -1,14 +1,13 @@
 import { createPool } from "mysql2/promise";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js";
 
-export const con = new createPool({
+/*export const con = new createPool({
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-});
-/*
+});*/
 //Para planet scape
 export const con = new createPool({
   host: "aws.connect.psdb.cloud",
@@ -16,7 +15,7 @@ export const con = new createPool({
   password: "pscale_pw_zpbtHA2oHtzXOx1NbhlJfTItdjwEVUQrZgcg30ylxiJ",
   database: "tasksbd",
   ssl: { rejectUnauthorized: false },
-});*/
+});
 /*
 export const con = new createPool({
     host: 'localhost',
